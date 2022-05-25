6746162f9614c22b363e3c977f53acc70b2fb1e1
Walentin <seryimir@gmail.com>
2022-05-26 01:12:39 +0300

    deleted localserver for api, apply unittest.mock

--- api_to_json.py
+++ api_to_json.py
@@ -1,22 +1,21 @@
 import requests
 import json
+from requests.exceptions import Timeout
 
 
 def get_api(url_api):
-	try:
-		return requests.get(url_api)
-	except requests.exceptions.ConnectionError as ce:
-		return f"url {url_api} not exist, run server", ce
+	return requests.get(url_api, timeout=5)
 
 
 def get_api_as_json(url_api):
 	response = get_api(url_api)
 	if response.status_code == 200:
 		return json.dumps(response.json())
-	return f'Can`t get api on url: {url_api}'
+	raise Exception(f'Can`t get api on url: {url_api}')
 
 
 if __name__ == "__main__":
+	# api_loc = "http://localhost:5050/api_local"
 	api_cat = "https://catfact.ninja/fact"
 	cat = get_api_as_json(api_cat)
 	print(type(cat), cat)
--- unittest_api_to_json.py
+++ unittest_api_to_json.py
@@ -1,39 +1,30 @@
 import unittest
-import json
-from api_to_json import get_api_as_json, get_api
+from unittest.mock import Mock, MagicMock, patch
+from api_to_json import get_api_as_json
+from requests.exceptions import Timeout
 
 
-class TestApiToJson(unittest.TestCase):
-	def setUp(self):
-		self.api_err = "https://catfact.ninja/error"
-		self.api_local = "http://localhost:5050/api_local"
-
-	def test_not_get_api(self):
-		self.assertEqual(get_api_as_json(self.api_err), 
-			f'Can`t get api on url: {self.api_err}')
-
-	def test_get_json_api(self):
-		self.assertEqual(json.loads(get_api_as_json(self.api_local)),
-						{'name': 'Valentin',
-	                    'email': 'val.work@example.com',
-	                    'age': 30,
-	                    'married': True,
-	                    'skils': ['Python3', 'Flask', 'json'],
-	                    'some': None}
-						)
+class TestApiToJson(unittest.TestCase):	
+	@patch('api_to_json.get_api')
+	def test_get_api(self, mock_get_api):
+		mock_status_200 = Mock(status_code=200)		
+		mock_status_200.json.return_value = {'name': 'Valentin',
+											'age': 30,
+											'married': True,
+											'skils': ['Python3', 'Flask']}							
+		mock_get_api.return_value = mock_status_200
+		resp = get_api_as_json("http://localhost:5050/api_local")
+		self.assertIsInstance(resp,  str)
+		self.assertTrue(resp)
+		self.assertEqual(resp, '{"name": "Valentin", "age": 30, "married": true, "skils": ["Python3", "Flask"]}')
 
 
-class TestGetApi(unittest.TestCase):
-	def setUp(self):
-		self.api_err = "https://catfact.ninja/error"
-		self.api_local = "http://localhost:5050/api_local"
-
-	def test_get_response(self):
-		self.assertEqual(str(get_api(self.api_local)), '<Response [200]>')
-	
-	def test_not_get_responce(self):
-		self.assertFalse(isinstance(get_api(self.api_err), tuple))
-
+	@patch('api_to_json.get_api')
+	def test_not_get_api(self, mock_get_apierr):
+		mock_get_apierr = Mock()
+		mock_get_apierr.side_effect = Timeout("I'm tired of waiting")
+		self.assertRaises(Exception, get_api_as_json, "http://localhost:5050/api_local",
+		 	msg='blya')
 
 if __name__ == '__main__':
 	unittest.main()
